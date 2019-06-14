66679fac4c1cd0fc7ad9c9db67f0741e21f8e664
Tommyleerom <tommyleerom@gmail.com>
2019-06-14 13:12:37 +0300

    Create a subscription

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,44 @@
+class ProfilesController < ApplicationController
+before_action :set_user
+
+  def show
+  end
+
+  def subscribe
+    if current_user.id == @user.id
+      redirect_to profile_path(@user), notice: "You can't be subscribe on your self"
+  	else
+      if current_user.subscriptions.exists?(friend_id: @user.id)
+    	 	redirect_to profile_path(@user), notice: "You are already subscribe on this user"		
+    	else
+  	  	@subscription = current_user.subscriptions.build
+  	  	@subscription.friend_id = @user.id
+  	  	@subscription.save
+  		  	if @subscription.save
+  		  		redirect_to profile_path(@user), notice: "You are subscribe on this user"
+  		  	end
+    	end
+    end
+  end
+
+  def unsubscribe
+    if current_user.id == @user.id
+      redirect_to profile_path(@user), notice: "You can't unsubscribe from your self"
+    else
+      if current_user.subscriptions.exists?(friend_id: @user.id)
+    	@subscription = current_user.subscriptions.find_by_friend_id(@user.id)
+    	@subscription.destroy
+    	redirect_to profile_path(@user), notice: "You are unsubscribe this user"
+    	else
+    		redirect_to profile_path(@user), notice: "You are not subscribe on this user"		
+    	end
+    end
+  end
+
+  private
+
+  	def set_user
+      @user = User.find(params[:id])
+    end
+
+end
